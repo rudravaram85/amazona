986c38e2eddf61cff144fd9fd0f2654f6378ebc9
rudravaram85 <rajesh.rudravaram85@gmail.com>
2024-05-15 11:42:59 +0530

    rating comp

--- frontend/src/components/product.js
+++ frontend/src/components/product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Rating from './rating';
 
 function Product(props) {
   const { product } = props;
@@ -13,28 +14,10 @@ function Product(props) {
           <a href={`/product/${product._id}`}>
             <h2>{product.name}</h2>
           </a>
-          <div className="rating">
-            <span>
-              {' '}
-              <i className="fa fa-star"></i>{' '}
-            </span>
-            <span>
-              {' '}
-              <i className="fa fa-star"></i>{' '}
-            </span>
-            <span>
-              {' '}
-              <i className="fa fa-star"></i>{' '}
-            </span>
-            <span>
-              {' '}
-              <i className="fa fa-star"></i>{' '}
-            </span>
-            <span>
-              {' '}
-              <i className="fa fa-star"></i>{' '}
-            </span>
-          </div>
+          <Rating
+            rating={product.rating}
+            numReviews={product.numReviews}
+          ></Rating>
           <div className="price">${product.price}</div>
         </div>
       </div>
